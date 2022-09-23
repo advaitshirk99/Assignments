66e6d2b5ff99b2b41a24783ab352184b9f7250c8
advshirk99 <adshirvaikar@gmail.com>
2022-09-24 00:28:19 +0530

    program 4

--- RTOS/assignment1/prg4/main.c
+++ RTOS/assignment1/prg4/main.c
@@ -1,5 +1,30 @@
 #define MAX_LINE_LENGTH 1000
 #include<stdio.h>
+#include<stdlib.h>
+
+int give_words(char arr[]){
+	
+	int i, word_count =0;
+	for(i=0; arr[i] != '\0'; i++){
+		if(arr[i] == ' ' && arr[i+1] != ' '){
+			word_count++;
+		}
+	}
+	return word_count;
+}
+
+int *give_lines(char arr[], FILE* ptr, int *result){
+
+	int line_count=0, word_count=0;
+
+	while (fgets(arr, MAX_LINE_LENGTH, ptr)){
+		word_count += give_words(arr);
+		line_count++;
+	}
+	*(result) = line_count;
+	*(result+1) = word_count;
+	return result;
+}
 
 int main(){
 
@@ -12,16 +37,11 @@ int main(){
                 return 0;
         }
 
-	while (fgets(line, MAX_LINE_LENGTH, ptr)){
-		for(i=0; line[i] != '\0'; i++){
-			if(line[i] == ' ' && line[i+1] != ' '){
-				word_count++;
-			}
-		}
-		line_count++;
-	}
-
-	printf("Line count: %d\nWord Count: %d\n", line_count, word_count );
-
+	int *final_result, *result;
+	result = malloc(2 * sizeof(int));
+	final_result = give_lines(line, ptr, result);	
+	printf("Line count: %d\nWord Count: %d\n", result[0], result[1]);
+	
+	free(result);
 	return 0; 
 }
