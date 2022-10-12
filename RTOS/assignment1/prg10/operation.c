e7c8aa2b485374261446d857cfdce5f9cc32a96b
advaitshirk99 <shirvaikar.msismpl2022@learner.manipal.edu>
2022-10-12 21:12:44 +0530

    program 10, last word duplicated

--- RTOS/assignment1/prg10/operation.c
+++ RTOS/assignment1/prg10/operation.c
@@ -4,6 +4,7 @@
 #include<ctype.h>
 #include<string.h>
 
+//Function to change the word from lower case to upper case
 char* change_case(char arr[]){
 	
 	int i, size = strlen(arr);
@@ -15,53 +16,53 @@ char* change_case(char arr[]){
 	return arr;
 }
 
-void return_punctuation(char arr[]){
+//Function to get the punctuation attached to a word
+char return_punctuation(char arr[]){
 
 	int i, size = strlen(arr);
-
+	char e;
+	
 	for (i=0; i<size; i++){
-		
 		if(ispunct(arr[i])){
-			
-			arr[i]= '\0';
+			e = arr[i];	//Storing the punctuation to return it later		
+			arr[i]= '\0';	//Ending the char array here with the null character
+			return e;
 		}
 	}
 }
 
+//Function to read the file, and change the selected words into uppercase
 void lower_to_upper(FILE* source, FILE* destination){
 	
-	char read[20], words[][20] = {"Manipal", "Udupi", "miss", "names"};
+	int i;	
+	char punctuation, read[20], words[][20] = {"Manipal", "Udupi", "miss", "names"};
 	while(!feof(source)){
-			
+
 		fscanf(source, "%s", read);
 		
-		return_punctuation(read);	
+		punctuation = return_punctuation(read);	
+		int flag =0;
 
-		if(strcmp(read, words[0]) == 0) {
-			
-			fprintf(destination, "%s ", change_case(read));
-			fscanf(source, "%s", read);
+		for (i=0; i<4; i++){
+
+			if(strcmp(read, words[i]) == 0) {
+				if(ispunct(punctuation)){
+					fprintf(destination, "%s%c ", change_case(read), punctuation);
+				}
+				else fprintf(destination, "%s ", change_case(read));	 	
+				flag =1;
+			}
 		}
 
-		if(strcmp(read, words[1]) == 0) { 
-
-                        fprintf(destination, "%s ", change_case(read));
-			fscanf(source, "%s", read);
-		}
+		if (flag == 1) continue;
 		
-		if(strcmp(read, words[2]) == 0) { 
-
-                        fprintf(destination, "%s ", change_case(read));
-			fscanf(source, "%s", read);
-		}
-
-		if(strcmp(read, words[3]) == 0) { 
-
-                        fprintf(destination, "%s ", change_case(read));
-		}
-
 		else{
-			fprintf(destination, "%s ", read);
+			if(ispunct(punctuation)){
+				fprintf(destination, "%s%c ", read, punctuation);
+			}
+			else{
+				fprintf(destination, "%s ", read);	
+			}
 		}
 	}
 }
