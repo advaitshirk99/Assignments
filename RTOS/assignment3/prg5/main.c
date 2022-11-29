108e765865fbaf2a20cdb2ebf39c75676e8ab6db
advshirk99 <adshirvaikar@gmail.com>
2022-11-30 00:09:59 +0530

    updated

--- RTOS/assignment3/prg5/main.c
+++ RTOS/assignment3/prg5/main.c
@@ -1,8 +1,24 @@
 #include<stdio.h>
 #include<unistd.h>
-#include<stdlib.h>
+#include "functions.h"
 
 int main(){
+	int total, i;
+	float total_wt =0, total_tt=0;
+	printf("Enter number of processes to be considered: ");
+	scanf("%d", &total);
+	process_data process[total];
+	for(i=0; i<total; i++){
+		input_pdata(process[i]);
+	}
 	int split = fork();
-	if(split == 0)
+	if(split == 0){
+		fcfs(process);
+	}
+	for(i=0; i<total; i++){
+		total_wt +=	process[i].waiting_time;
+		total_tt += process[i].turn_around_time;  
+	}
+	printf("FOR FCFS:\nAverage waiting time: %d\nAverage turnaround time: %d\n",(total_wt/(float)total), (total_tt/(float)total));
+
 }
